54d111805b8a297aeba60c57052af470961209c7
Sarvesh Hadole <hadolesarvesh031@gmail.com>
2024-06-08 19:54:51 +0530

    Update truffle-config.js

--- marketplace/truffle-config.js
+++ marketplace/truffle-config.js
@@ -4,8 +4,8 @@ require('babel-polyfill');
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "",
+      port: ,
       network_id: "*" // Match any network id
     },
   },
