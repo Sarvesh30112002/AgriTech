47f28be0e0c7a559916c84cde39616c61e338fcb
Sarvesh Hadole <hadolesarvesh031@gmail.com>
2024-06-08 20:03:10 +0530

    Update users.model.js

--- node_server/models/users.model.js
+++ node_server/models/users.model.js
@@ -15,14 +15,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: 3
-  },
-  mobile: {
-    type: String
-  },
+  
   coins: {
     type: Number,
     default: 0
